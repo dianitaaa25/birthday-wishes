9de8bfc156ba195b913886b6d60a7940afe8e897
Samil Acosta <159671384+dianitaaa25@users.noreply.github.com>
2024-06-25 01:04:38 -0400

    Update memory.js

--- script/memory.js
+++ script/memory.js
@@ -1,5 +1,22 @@
-animationTimeline();
-
+// trigger to play music in the background with sweetalert
+window.addEventListener('load', () => {
+    Swal.fire({
+        title: 'Se va a reproducir música de fondo.',
+        icon: 'warning',
+        showCancelButton: true,
+        //confirmButtonColor: '#3085d6',
+        //cancelButtonColor: '#d33',
+        //confirmButtonText: 'Sí',
+        //cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector('.song').play(); // Reproducir música solo si se confirma "Sí"
+            animationTimeline();
+        } else {
+            animationTimeline();
+        }
+    });
+});
 
 
 // animation timeline
